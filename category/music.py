66c553c62fd8189dd37690f58ac48638746d43f1
tsb <mpalts1234@hotmail.com>
2020-02-15 17:23:55 +0800

    music

--- category/music.py
+++ category/music.py
@@ -8,12 +8,18 @@ class Music(commands.Cog, name="Music"):
         self.client = client
         self.queue = {}
 
-    @commands.command(name="play", usage="<youtube url>")
+    @commands.command(name="play", usage="<youtube url> / <search term>")
     @commands.cooldown(1, 5, commands.BucketType.user)
-    async def play(self, ctx, url: str):
-        if not ctx.author.voice.channel:
+    async def play(self, ctx, *, term):
+        if ctx.author.voice is None:
             await ctx.send(embed=funcs.errorEmbed(None, "Please connect to a voice channel."))
             return
+
+    async def init(self, ctx, url):
+        pass
+
+
+class YTDLSource()
 
 
 def setup(client):
