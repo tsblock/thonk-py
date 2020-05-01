14ff8a0bbd696700549987503c63c488d082dfec
tsblock <mpalts1234@hotmail.com>
2020-05-01 16:35:39 +0800

    added something

--- category/music.py
+++ category/music.py
@@ -1,3 +1,6 @@
+from typing import Dict, List
+
+import discord
 from discord.ext import commands
 
 from utils import funcs
@@ -6,7 +9,7 @@ from utils import funcs
 class Music(commands.Cog, name="Music"):
     def __init__(self, client):
         self.client = client
-        self.queue = {}
+        self.queue = Dict[int, List[SongEntry]]
 
     @commands.command(name="play", usage="<youtube url> / <search term>")
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -19,7 +22,18 @@ class Music(commands.Cog, name="Music"):
         pass
 
 
-class YTDLSource()
+class SongEntry(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data):
+        super().__init__(source, volume=0.5)
+
+        self.data = data  # youtube video data
+
+        self.title = data.get("title")
+        self.url = data.get("url")
+
+    @classmethod
+    async def getPCMAudio(cls, url):
+        pass
 
 
 def setup(client):
