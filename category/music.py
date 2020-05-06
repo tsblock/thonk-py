8ab7a859f3344fc8da6be3d4094e86ee1c2544c2
tsblock <mpalts1234@hotmail.com>
2020-05-06 18:57:14 +0800

    ytdl source class

--- category/music.py
+++ category/music.py
@@ -1,17 +1,39 @@
+import asyncio
 from typing import Dict, List
 
+import discord
 import youtube_dl
 from discord.ext import commands
 
 from utils import funcs
 
-ytdl = youtube_dl.YoutubeDL()
+ytdl_options = {
+    'format': 'bestaudio/best',
+    'extractaudio': True,
+    'audioformat': 'mp3',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0',
+}
+
+ffmpeg_options = {
+    'options': '-vn'  # disable video
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_options)
 
 
 class Music(commands.Cog, name="Music"):
     def __init__(self, client):
         self.client = client
-        self.queue = Dict[int, List[SongEntry]]
+        self.queue = Dict[int, List[YTDLSource]]
 
     @commands.command(name="play", usage="<youtube url> / <search term>")
     @commands.cooldown(1, 5, commands.BucketType.user)
@@ -20,8 +42,30 @@ class Music(commands.Cog, name="Music"):
             await ctx.send(embed=funcs.errorEmbed(None, "Please connect to a voice channel."))
             return
 
-    async def init(self, ctx, url):
-        pass
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        # video info
+        self.title = data.get("title")
+        self.url = data.get("url")
+        self.length = data.get("duration")
+
+    @classmethod
+    async def from_url(cls, url, *, event_loop=None, stream=False):
+        event_loop = event_loop or asyncio.get_event_loop()
+        data = await event_loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            # TODO: implement playlist import
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 
 def setup(client):
