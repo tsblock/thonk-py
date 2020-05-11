0801851d0b14b7151bad64fc4acdee3cc1a6467d
tsblock <mpalts1234@hotmail.com>
2020-05-11 22:35:23 +0800

    wtf i commited in the wrong branch

--- category/administrator.py
+++ category/administrator.py
@@ -97,7 +97,7 @@ class Administrator(commands.Cog, name="Administrator"):
                 await ctx.send("k")
                 if msg.content == "git pull":
                     output = subprocess.check_output(["git", "pull"])
-                    output = output.decode("unicode_escape")
+                    output = output.decode("string_escape")
                     await ctx.send("```{}```".format(output))
                 sys.exit()
 
