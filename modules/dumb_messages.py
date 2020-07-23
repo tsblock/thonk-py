029ca695263843801ca7d694328de05b4acfa82e
tsblock <mpalts1234@hotmail.com>
2020-07-23 21:53:50 +0800

    f msg bug fixed

--- modules/dumb_messages.py
+++ modules/dumb_messages.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 
 from database import guild_settings
 
-f_file = open("assets/f.jpg", "rb")
-
 
 class dumb_messages(commands.Cog, name="dumb messages"):
     def __init__(self, client):
@@ -15,9 +13,10 @@ class dumb_messages(commands.Cog, name="dumb messages"):
         content = ctx.content
         if ctx.guild is None:
             return
-        if content.lower() == "f":
+        if content.casefold() == "f":
             guild_setting = guild_settings.get(ctx.guild.id)
             if guild_setting.dumb_message:
+                f_file = open("assets/f.jpg", "rb")
                 discord_file = discord.File(fp=f_file)
                 await ctx.channel.send(file=discord_file)
 
