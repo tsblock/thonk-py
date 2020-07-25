d5174877067f48e65c026e66a089a06ff27d5769
tsblock <mpalts1234@hotmail.com>
2020-07-25 23:19:30 +0800

    16craft ping 4 ryan

---
+++ modules/16craft.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+
+
+class SixteenCraft(commands.Cog, name="16craft"):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_message(self, ctx):
+        if ctx.author.id == "597028739616079893" and ctx.channel.id == "612962167284695040":
+            await self.client.get_channel("698924656253599765").send(
+                "<@161973479858503680> server down fdskjflkdsfjdslkfdsk;l")
+
+
+def setup(client):
+    client.add_cog(client)
