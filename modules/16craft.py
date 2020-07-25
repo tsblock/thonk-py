95fa982332d6195e35b2dc8aac105b52350d1e6a
tsblock <mpalts1234@hotmail.com>
2020-07-25 23:23:18 +0800

    16craft event fixofdsfds

--- modules/16craft.py
+++ modules/16craft.py
@@ -7,10 +7,10 @@ class SixteenCraft(commands.Cog, name="16craft"):
 
     @commands.Cog.listener()
     async def on_message(self, ctx):
-        if ctx.author.id == "597028739616079893" and ctx.channel.id == "612962167284695040":
+        if ctx.author.id == "597028739616079893" and ctx.channel.id == "612962167284695040" and ctx.message.content == "**Server has stopped.** :octagonal_sign:":
             await self.client.get_channel("698924656253599765").send(
                 "<@161973479858503680> server down fdskjflkdsfjdslkfdsk;l")
 
 
 def setup(client):
-    client.add_cog(client)
+    client.add_cog(SixteenCraft(client))
