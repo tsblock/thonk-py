b016834b66ab270dc5514735428c075d42808be2
tsb <mpalts1234@hotmail.com>
2021-02-12 00:20:23 +0800

    coinprice cmd: minor minor change; round up numbers

--- modules/cryptocurrency.py
+++ modules/cryptocurrency.py
@@ -43,6 +43,7 @@ class CryptoCurrency(commands.Cog, name="Cryptocurrency"):
                 await ctx.send(embed=funcs.error_embed(None, "Invalid fiat currency"))
                 return
             fiat_currency = fiat_currency.upper()
+
             name = market_info["name"]
             thumbnail = market_info["image"]
             current_price = str(market_info["current_price"]) + " " + fiat_currency
@@ -51,14 +52,13 @@ class CryptoCurrency(commands.Cog, name="Cryptocurrency"):
             circulating_supply = str(market_info["circulating_supply"])
             max_supply = str(market_info["max_supply"])
             ath = str(market_info["ath"]) + " " + fiat_currency
-            atl = str(market_info["atl"]) + " " + fiat_currency
-            price_change_percentage_1h = str(market_info["price_change_percentage_1h_in_currency"]) + "%"
-            price_change_percentage_24h = str(market_info["price_change_percentage_24h_in_currency"]) + "%"
-            price_change_percentage_7d = str(market_info["price_change_percentage_7d_in_currency"]) + "%"
-            price_change_percentage_1y = str(market_info["price_change_percentage_1y_in_currency"]) + "%"
+            price_change_percentage_1h = str(round(market_info["price_change_percentage_1h_in_currency"], 4)) + "%"
+            price_change_percentage_24h = str(round(market_info["price_change_percentage_24h_in_currency"], 4)) + "%"
+            price_change_percentage_7d = str(round(market_info["price_change_percentage_7d_in_currency"], 4)) + "%"
+            price_change_percentage_1y = str(round(market_info["price_change_percentage_1y_in_currency"], 4)) + "%"
             last_updated = market_info["last_updated"]
-
             color = bull_or_bear(price_change_percentage_24h)
+
             market_info_embed = discord.Embed(color=color)
             market_info_embed.set_author(name=name, icon_url=thumbnail)
             market_info_embed.add_field(name="💵 Current Price", value=f"`{current_price}`")
@@ -73,6 +73,7 @@ class CryptoCurrency(commands.Cog, name="Cryptocurrency"):
             market_info_embed.add_field(name="📊 Price Change (1y)", value=f"`{price_change_percentage_1y}`")
             market_info_embed.set_footer(text="Data provided by CoinGecko", icon_url="https://i.imgur.com/7nzhHrl.png")
             market_info_embed.timestamp = datetime.strptime(last_updated, "%Y-%m-%dT%H:%M:%S.%fZ")
+
             await ctx.send(embed=market_info_embed)
         else:
             await ctx.send(embed=funcs.error_embed("Invalid coin ticker!", "Be sure to use the ticker. (e.g. `btc`)"))
